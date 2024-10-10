e7dc435f27730eed274d2d29f226872af91b5c42
Dipankar Mnadal <dipankar0535@gmail.com>
2024-10-10 23:20:13 +0530

    changes

--- UI/grocery/src/Navbar.jsx
+++ UI/grocery/src/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './static/logo-grocery.png'; // Make sure the path is correct
-import { Link } from 'react-router-dom'; // Import the Link component
+import logo from './static/logo-grocery.png'; 
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return(
@@ -10,10 +10,20 @@ const Navbar = () => {
                     <img src={logo} alt="Grocery-image" />
                 </div>
                 <div className="nav-links">
-                    {/* Use Link instead of a tags */}
+                    {/* Use Link instead of a tags
                     <Link to="/">Home</Link>
                     <Link to="/about">About</Link>
-                    <Link to="/login">Login</Link>
+                    <Link to="/login">Login</Link> */}
+
+                    <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
+                        Home
+                    </NavLink>
+                    <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>
+                        About
+                    </NavLink>
+                    <NavLink to="/login" className={({ isActive }) => (isActive ? "active" : "")}>
+                        Login
+                    </NavLink>
                 </div>
             </div>
         </nav>
