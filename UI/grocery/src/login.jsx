932b53415dcf4c7f247b0450bdc05df3867b3e65
Dipankar Mnadal <dipankar0535@gmail.com>
2024-10-18 21:40:27 +0530

    login box

--- UI/grocery/src/login.jsx
+++ UI/grocery/src/login.jsx
@@ -6,10 +6,12 @@ function Login(){
 
         <div class = "container">
             <div class = "container-header"> Login </div>
+            <hr />
             <div class = "container-body">
                 <label htmlFor="email" id = "container-label"> Username/Email </label>
-                {/* <span id="user-email-hint" class="input-hint"></span> */}
                 <input type="text" id = "container-input" placeholder="Example: jane@sample.com" />
+                <label htmlFor="password" id = "container-label"> Password </label>
+                <input type="password" id = "container-input" placeholder="Password" />
             </div>
         </div>
 
