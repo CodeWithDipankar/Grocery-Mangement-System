ae3eeddfe708983d2c5cb20d9992373cf6f83709
Mamoshree.Saha <sahamamoshree123@gmail.com>
2024-10-20 00:37:54 +0530

    1) Changes added for navigation bar 2) Added dynamic navigation for navbar using useNavigate() hook

--- UI/grocery/src/App.jsx
+++ UI/grocery/src/App.jsx
@@ -1,15 +1,38 @@
-import Login from "./login";
-import './Home.css'
-import NavBar from './components/navbar'
+// import Login from "./login";
+// import './Home.css'
+// import NavBar from './components/navbar'
 
-function App(){
-  return(
-    <div class = "main-app">
-      <NavBar />
-      <Login />
-    </div>
+// function App(){
+//   return(
+//     <div class = "main-app">
+//       <NavBar />
+//       <Login />
+//     </div>
     
-  )
+//   )
+// }
+
+// export default App;
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Login from './login';
+import Navbar from './components/navbar'
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
+
