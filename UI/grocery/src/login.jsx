65437f4cf56c6a10355fe5f2aad73a7c67883229
Dipankar Mnadal <dipankar0535@gmail.com>
2024-10-19 22:05:32 +0530

    new css

--- UI/grocery/src/login.jsx
+++ UI/grocery/src/login.jsx
@@ -4,18 +4,20 @@ function Login() {
     return (
         <div className="container">
             <div className="container-header">Login</div>
-            <hr />
+            <hr class="divider"></hr>
             <div className="container-body">
-                <label htmlFor="email" className="container-label">Username/Email</label>
+                <label htmlFor="email" className="container-label">Username</label>
                 <input type="text" id="email" className="container-input" placeholder="Example: jane@sample.com" />
                 <label htmlFor="password" className="container-label">Password</label>
                 <input type="password" id="password" className="container-input" placeholder="Password" />
                 <button className="btn">Submit</button>
             </div>
+            <hr class="divider"></hr>
             <div className="container-footer">
                 <p>Forgot password? <a href="#">Click here</a></p>
             </div>
         </div>
+        
     );
 }
 
