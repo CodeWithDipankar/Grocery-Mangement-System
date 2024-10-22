f0c4616335248c48e4b42b1cf9209a8b0d8858fa
Mamoshree.Saha <sahamamoshree123@gmail.com>
2024-10-22 23:12:31 +0530

    Added forgot password page from login page

--- UI/grocery/src/App.jsx
+++ UI/grocery/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import About from './components/About';
 import Login from './components/login';
 import Navbar from './components/navbar'
+import ForgotPassword from './components/forgotPassword';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './style/saas/custom.scss'; 
 import './style/css/override.css';
@@ -17,6 +18,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/forgot_password" element={<ForgotPassword />} />
             </Routes>
         </Router>
     );
