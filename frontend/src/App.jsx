fef75bf119b874a7a80a0bded6116bb9c5daa567
Dipankar Mandal <dipankar0535@gmail.com>
2024-10-25 00:05:02 +0530

    changes for today ... happy flights ..

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <h1 className="text-2xl font-bold text-center font-mono">
+      Hello world!
+      I am developing a New frontend Using Tailwind CSS which is utility framework.
+    </h1>
+  )
+}
